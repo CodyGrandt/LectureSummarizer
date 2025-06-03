24051ee5a80990ecf056ec9ef0abad1bc6e6cf5a
CodyGrandt <codygrandt04@gmail.com>
2025-06-02 22:35:30 -0400

    more backend changes.

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,10 @@ app = FastAPI()
 # Enable CORS for local frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://codygrandt.github.io"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
